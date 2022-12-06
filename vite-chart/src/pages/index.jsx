1904cd6699eac2af13b29b406212dc2ec9f95fd4
Liu.Yoga <faaa@live.com>
2022-12-06 19:05:55 +0800

    update 修改云图

--- vite-chart/src/pages/index.jsx
+++ vite-chart/src/pages/index.jsx
@@ -142,45 +142,162 @@ let wordCloudOption = {
   tooltip: {
     show: true,
   },
+  // series: [
+  //   {
+  //     name: '高频风险项',
+  //     type: 'wordCloud',
+  //     size: ['9%', '99%'],
+  //     sizeRange: [6, 40],
+  //     textRotation: [0, 45, 90, -45],
+  //     rotationRange: [-45, 90],
+  //     //shape: 'circle',
+  //     textPadding: 0,
+  //     autoSize: {
+  //       enable: true,
+  //       minSize: 6,
+  //     },
+  //     textStyle: {
+  //       normal: {
+  //         color: function () {
+  //           return (
+  //             'rgb(' +
+  //             [
+  //               Math.round(Math.random() * 160),
+  //               Math.round(Math.random() * 160),
+  //               Math.round(Math.random() * 160),
+  //             ].join(',') +
+  //             ')'
+  //           );
+  //         },
+  //       },
+  //       emphasis: {
+  //         shadowBlur: 10,
+  //         shadowColor: '#333',
+  //       },
+  //     },
+  //     top: '15%',
+  //     left: '5%',
+  //     right: '1%',
+  //     bottom: '1%',
+  //     width: '90%',
+  //     data: [],
+  //   },
+  // ],
   series: [
     {
-      name: '高频风险项',
       type: 'wordCloud',
-      size: ['9%', '99%'],
-      sizeRange: [6, 40],
-      textRotation: [0, 45, 90, -45],
-      rotationRange: [-45, 90],
-      //shape: 'circle',
-      textPadding: 0,
-      autoSize: {
-        enable: true,
-        minSize: 6,
-      },
+      gridSize: 20,
+      sizeRange: [12, 50],
+      rotationRange: [0, 0],
+      shape: 'circle',
       textStyle: {
-        normal: {
-          color: function () {
-            return (
-              'rgb(' +
-              [
-                Math.round(Math.random() * 160),
-                Math.round(Math.random() * 160),
-                Math.round(Math.random() * 160),
-              ].join(',') +
-              ')'
-            );
-          },
+        color: function () {
+          return (
+            'rgb(' +
+            [
+              Math.round(Math.random() * 160),
+              Math.round(Math.random() * 160),
+              Math.round(Math.random() * 160),
+            ].join(',') +
+            ')'
+          );
         },
+
         emphasis: {
           shadowBlur: 10,
           shadowColor: '#333',
         },
       },
-      top: '15%',
-      left: '5%',
-      right: '1%',
-      bottom: '1%',
-      width: '90%',
-      data: [],
+      data: [
+        {
+          name: 'Sam S Club',
+          value: 10000,
+          textStyle: {
+            normal: {
+              color: 'black',
+            },
+            emphasis: {
+              color: 'red',
+            },
+          },
+        },
+        {
+          name: 'Macys',
+          value: 6181,
+        },
+        {
+          name: 'Amy Schumer',
+          value: 4386,
+        },
+        {
+          name: 'Jurassic World',
+          value: 4055,
+        },
+        {
+          name: 'Charter Communications',
+          value: 2467,
+        },
+        {
+          name: 'Chick Fil A',
+          value: 2244,
+        },
+        {
+          name: 'Planet Fitness',
+          value: 1898,
+        },
+        {
+          name: 'Pitch Perfect',
+          value: 1484,
+        },
+        {
+          name: 'Express',
+          value: 1112,
+        },
+        {
+          name: 'Home',
+          value: 965,
+        },
+        {
+          name: 'Johnny Depp',
+          value: 847,
+        },
+        {
+          name: 'Lena Dunham',
+          value: 582,
+        },
+        {
+          name: 'Lewis Hamilton',
+          value: 555,
+        },
+        {
+          name: 'KXAN',
+          value: 550,
+        },
+        {
+          name: 'Mary Ellen Mark',
+          value: 462,
+        },
+        {
+          name: 'Farrah Abraham',
+          value: 366,
+        },
+        {
+          name: 'Rita Ora',
+          value: 360,
+        },
+        {
+          name: 'Serena Williams',
+          value: 282,
+        },
+        {
+          name: 'NCAA baseball tournament',
+          value: 273,
+        },
+        {
+          name: 'Point Break',
+          value: 265,
+        },
+      ],
     },
   ],
 };
